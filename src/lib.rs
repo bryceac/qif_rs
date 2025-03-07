dd2607e7e7626fd4b69f266e6b3815b3f6a1a7a9
Bryce Campbell <tonyhawk2100@gmail.com>
2025-03-07 08:01:58 -0800

    fix test code.

--- src/lib.rs
+++ src/lib.rs
@@ -684,8 +684,7 @@ mod tests {
         .set_field(bank_section)
         .build();
 
-        if let Ok(retrieved_qif) = QIF::load_from_file("test.qif") {
-            assert_eq!(retrieved_qif, qif)
-        }
+        let retrieved_qif = QIF::load_from_file("test.qif");
+        assert_eq!(retrieved_qif.unwrap(), qif)
     }
 }
